f526ebc93b7cebb75079f7676a75930e9c434db5
Ciprian <ciliesj@gmail.com>
2017-01-11 23:30:17 -0800

    Updated game version to v2.0.5

--- src/game.hpp
+++ src/game.hpp
@@ -12,7 +12,7 @@
 #pragma once
 
 // REMEMBER TO CHANGE THIS WITH EVERY NEW OFFICIAL VERSION!!!
-#define VERSION "v2.0.4"
+#define VERSION "v2.0.5"
 #define GAME_CODE
 
 #define MAX_FPS_LIMIT 60 //TODO: Make this configurable.
