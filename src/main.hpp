b90c8cedca6c1871265ee767da3b43eca98e9d18
WALL OF JUSTICE <->
2017-03-12 18:16:37 +1100

    Language entries raised to 2500

--- src/main.hpp
+++ src/main.hpp
@@ -437,7 +437,7 @@ extern int minotaurlevel;
 #define DIRECTCLIENT 4
 
 // language stuff
-#define NUMLANGENTRIES 2000
+#define NUMLANGENTRIES 2500
 extern char languageCode[32];
 extern char** language;
 
