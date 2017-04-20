b3d891a379385c4f5778e5b053e2c74838ab2df9
Ciprian <ciliesj@gmail.com>
2017-04-19 17:35:41 -0700

    Fix comments.

--- src/colors.hpp
+++ src/colors.hpp
@@ -11,17 +11,20 @@
 
 #pragma once
 
-//SDL_Color colors.
+/*
+ * SDL_Color colors.
+ */
 static const SDL_Color sdlColorWhite = { 255, 255, 255, 255 };
 
 
-//Generates the corresponding color Uint32s from the given surface.
+/*
+ * The following functions generate the corresponding color Uint32s from the given surface.
+ */
 inline Uint32 uint32ColorWhite(const SDL_Surface& surface)
 {
 	return SDL_MapRGB(surface.format, 255, 255, 255);
 }
 
-//Generates the corresponding color Uint32s from the given surface.
 inline Uint32 uint32ColorGray(const SDL_Surface& surface)
 {
 	return SDL_MapRGB(surface.format, 127, 127, 127);
