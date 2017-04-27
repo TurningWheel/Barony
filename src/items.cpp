59fda5a7b9b1a78f3211d5b06a5e553f2204d792
Ciprian <ciliesj@gmail.com>
2017-04-27 00:37:50 -0700

    Created new file item_tool.cpp that implements item category tools. Split off skeleton key's and lockpick's usage funcs into said file.

--- src/items.cpp
+++ src/items.cpp
@@ -2130,6 +2130,11 @@ int Item::sellValue(int player)
 
 void Item::apply(int player, Entity* entity)
 {
+	if ( !entity )
+	{
+		return;
+	}
+
 	// for clients:
 	if ( multiplayer == CLIENT )
 	{
@@ -2152,165 +2157,12 @@ void Item::apply(int player, Entity* entity)
 	// effects
 	if ( type == TOOL_SKELETONKEY )
 	{
-		// skeleton key
-		if ( entity->behavior == &actChest )
-		{
-			playSoundEntity(entity, 91, 64);
-			if ( entity->skill[4] )
-			{
-				messagePlayer(player, language[1097]);
-				entity->skill[4] = 0;
-			}
-			else
-			{
-				messagePlayer(player, language[1098]);
-				entity->skill[4] = 1;
-			}
-		}
-		else if ( entity->behavior == &actDoor )
-		{
-			playSoundEntity(entity, 91, 64);
-			if ( entity->skill[5] )
-			{
-				messagePlayer(player, language[1099]);
-				entity->skill[5] = 0;
-			}
-			else
-			{
-				messagePlayer(player, language[1100]);
-				entity->skill[5] = 1;
-			}
-		}
-		else
-		{
-			messagePlayer(player, language[1101], getName());
-		}
+		applySkeletonKey(player, *entity);
 	}
 	else if ( type == TOOL_LOCKPICK )
 	{
 		// lockpicks
-		if ( entity->behavior == &actChest )
-		{
-			if ( entity->skill[4] )
-			{
-				if ( stats[player]->PROFICIENCIES[PRO_LOCKPICKING] > rand() % 400 )
-				{
-					playSoundEntity(entity, 91, 64);
-					messagePlayer(player, language[1097]);
-					entity->skill[4] = 0;
-					players[player]->entity->increaseSkill(PRO_LOCKPICKING);
-				}
-				else
-				{
-					playSoundEntity(entity, 92, 64);
-					messagePlayer(player, language[1102]);
-					if (rand() % 10 == 0)
-					{
-						players[player]->entity->increaseSkill(PRO_LOCKPICKING);
-					}
-					else
-					{
-						if ( rand() % 5 == 0 )
-						{
-							if ( player == clientnum )
-							{
-								if ( count > 1 )
-								{
-									newItem(type, status, beatitude, count - 1, appearance, identified, &stats[player]->inventory);
-								}
-							}
-							stats[player]->weapon->count = 1;
-							stats[player]->weapon->status = static_cast<Status>(stats[player]->weapon->status - 1);
-							if ( status != BROKEN )
-							{
-								messagePlayer(player, language[1103]);
-							}
-							else
-							{
-								messagePlayer(player, language[1104]);
-							}
-							if ( player > 0 && multiplayer == SERVER )
-							{
-								strcpy((char*)net_packet->data, "ARMR");
-								net_packet->data[4] = 5;
-								net_packet->data[5] = stats[player]->weapon->status;
-								net_packet->address.host = net_clients[player - 1].host;
-								net_packet->address.port = net_clients[player - 1].port;
-								net_packet->len = 6;
-								sendPacketSafe(net_sock, -1, net_packet, player - 1);
-							}
-						}
-					}
-				}
-			}
-			else
-			{
-				messagePlayer(player, language[1105]);
-			}
-		}
-		else if ( entity->behavior == &actDoor )
-		{
-			if ( entity->skill[5] )
-			{
-				if ( stats[player]->PROFICIENCIES[PRO_LOCKPICKING] > rand() % 400 )
-				{
-					playSoundEntity(entity, 91, 64);
-					messagePlayer(player, language[1099]);
-					entity->skill[5] = 0;
-					players[player]->entity->increaseSkill(PRO_LOCKPICKING);
-				}
-				else
-				{
-					playSoundEntity(entity, 92, 64);
-					messagePlayer(player, language[1106]);
-					if ( rand() % 10 == 0 )
-					{
-						players[player]->entity->increaseSkill(PRO_LOCKPICKING);
-					}
-					else
-					{
-						if ( rand() % 5 == 0 )
-						{
-							if ( player == clientnum )
-							{
-								if ( count > 1 )
-								{
-									newItem(type, status, beatitude, count - 1, appearance, identified, &stats[player]->inventory);
-								}
-							}
-							stats[player]->weapon->count = 1;
-							stats[player]->weapon->status = static_cast<Status>(stats[player]->weapon->status - 1);
-							if ( status != BROKEN )
-							{
-								messagePlayer(player, language[1103]);
-							}
-							else
-							{
-								messagePlayer(player, language[1104]);
-							}
-							if ( player > 0 && multiplayer == SERVER )
-							{
-								strcpy((char*)net_packet->data, "ARMR");
-								net_packet->data[4] = 5;
-								net_packet->data[5] = stats[player]->weapon->status;
-								net_packet->address.host = net_clients[player - 1].host;
-								net_packet->address.port = net_clients[player - 1].port;
-								net_packet->len = 6;
-								sendPacketSafe(net_sock, -1, net_packet, player - 1);
-							}
-						}
-					}
-				}
-			}
-			else
-			{
-				messagePlayer(player, language[1107]);
-			}
-		}
-		else
-		{
-			messagePlayer(player, language[1101], getName());
-		}
+		applyLockpick(player, *entity);
 	}
 }
 
