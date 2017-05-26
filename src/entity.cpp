ec7334818510e337568e5b41cd172c9621a14d14
WALL OF JUSTICE <->
2017-05-26 20:26:18 +1000

    Fix random level ups doubling up a stat rarely.

--- src/entity.cpp
+++ src/entity.cpp
@@ -1483,7 +1483,23 @@ void Entity::handleEffects(Stat* myStats)
 		myStats->MP = std::min(myStats->MP, myStats->MAXMP);
 
 		// now pick three attributes to increase
+		// changed rolls to be unique for each possibility.
 		increasestat[0] = rand() % 6;
+		int r = rand() % 6;
+		while ( r == increasestat[0] ) {
+			r = rand() % 6;
+		}
+		increasestat[1] = r;
+		r = rand() % 6;
+		while ( r == increasestat[0] || r == increasestat[1] ) {
+			r = rand() % 6;
+		}
+		increasestat[2] = r;
+
+		// debug
+		// messagePlayer(0, "Stats rolled: %d %d %d", increasestat[0], increasestat[1], increasestat[2]);
+
+		/*increasestat[0] = rand() % 6;
 		increasestat[1] = rand() % 5;
 		increasestat[2] = rand() % 4;
 		if ( increasestat[1] >= increasestat[0] )
@@ -1497,7 +1513,7 @@ void Entity::handleEffects(Stat* myStats)
 		if ( increasestat[2] >= increasestat[1] )
 		{
 			increasestat[2]++;
-		}
+		}*/
 		for ( i = 0; i < 3; i++ )
 		{
 			messagePlayerColor(player, color, language[623 + increasestat[i]]);
