433e3d17214aa7b17ef7ad9a2dadedef7e97af08
WALL OF JUSTICE <->
2017-06-04 23:09:16 +1000

    Remove trap message from summon trap.. to restore at some point and notify if player triggered

--- src/actsummontrap.cpp
+++ src/actsummontrap.cpp
@@ -82,11 +82,11 @@ void actSummonTrap(Entity* my)
 				{
 					if ( SUMMONTRAP_MONSTER < KOBOLD )
 					{
-						messagePlayer(clientnum, language[2352], language[90 + SUMMONTRAP_MONSTER]);
+						//messagePlayer(clientnum, language[2352], language[90 + SUMMONTRAP_MONSTER]);
 					}
 					else if ( SUMMONTRAP_MONSTER >= KOBOLD )
 					{
-						messagePlayer(clientnum, language[2352], language[2000 + (SUMMONTRAP_MONSTER - 21)]);
+						//messagePlayer(clientnum, language[2352], language[2000 + (SUMMONTRAP_MONSTER - 21)]);
 					}
 					SUMMONTRAP_INITIALIZED = 1; // trap is starting up for the first time.
 				}
