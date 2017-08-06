17718a5f7b09fd962ce49b0f3957e352797e3f58
WALL OF JUSTICE <->
2017-08-06 21:21:26 +1000

    + Attack animations cockatrice TODO - item and stat up

--- src/monster_cockatrice.cpp
+++ src/monster_cockatrice.cpp
@@ -19,24 +19,28 @@
 #include "net.hpp"
 #include "collision.hpp"
 #include "player.hpp"
+#include "magic\magic.hpp"
 
 void initCockatrice(Entity* my, Stat* myStats)
 {
-	int c;
 	node_t* node;
 
 	my->initMonster(413);
 
 	if ( multiplayer != CLIENT )
 	{
-		MONSTER_SPOTSND = 198;
+		MONSTER_SPOTSND = 79;
+		MONSTER_SPOTVAR = 1;
+		MONSTER_IDLESND = -1;
+		MONSTER_IDLEVAR = 1;
+		/*MONSTER_SPOTSND = 198;
 		MONSTER_SPOTVAR = 3;
 		MONSTER_IDLESND = 201;
-		MONSTER_IDLEVAR = 3;
+		MONSTER_IDLEVAR = 3;*/
 	}
 	if ( multiplayer != CLIENT && !MONSTER_INIT )
 	{
-		if ( myStats != NULL )
+		if ( myStats != nullptr )
 		{
 			if ( !myStats->leader_uid )
 			{
@@ -52,8 +56,8 @@ void initCockatrice(Entity* my, Stat* myStats)
 			// boss variants
 
 			// random effects
-			myStats->EFFECTS[EFF_LEVITATING] = false;
-			myStats->EFFECTS_TIMERS[EFF_LEVITATING] = 0;
+			//myStats->EFFECTS[EFF_LEVITATING] = true;
+			//myStats->EFFECTS_TIMERS[EFF_LEVITATING] = 0;
 
 			if ( rand() % 4 == 0 )
 			{
@@ -82,19 +86,21 @@ void initCockatrice(Entity* my, Stat* myStats)
 				case 3:
 				case 2:
 				case 1:
-					if ( rand() % 4 == 0 )
+					/*if ( rand() % 4 == 0 )
 					{
 						newItem(static_cast<ItemType>(SPELLBOOK_FORCEBOLT + rand() % 21), static_cast<Status>(1 + rand() % 4), -1 + rand() % 3, 1, rand(), false, &myStats->inventory);
-					}
+					}*/
+
 					break;
 				default:
 					break;
 			}
 
+			newItem(SPELLBOOK_STONEBLOOD, DECREPIT, 0, 1, MONSTER_ITEM_UNDROPPABLE_APPEARANCE, false, &myStats->inventory);
 			//give weapon
-			if ( myStats->weapon == NULL && myStats->EDITOR_ITEMS[ITEM_SLOT_WEAPON] == 1 )
+			if ( myStats->weapon == nullptr && myStats->EDITOR_ITEMS[ITEM_SLOT_WEAPON] == 1 )
 			{
-				myStats->weapon = newItem(SPELLBOOK_FIREBALL, EXCELLENT, 0, 1, 0, false, NULL);
+				//myStats->weapon = newItem(SPELLBOOK_FIREBALL, EXCELLENT, 0, 1, 0, false, nullptr);
 			}
 		}
 	}
@@ -256,9 +262,12 @@ void cockatriceDie(Entity* my)
 void cockatriceMoveBodyparts(Entity* my, Stat* myStats, double dist)
 {
 	node_t* node;
-	Entity* entity = NULL;
-	Entity* rightbody = NULL;
-	int bodypart;
+	Entity* entity = nullptr;
+	Entity* leftbody = nullptr;
+	Entity* leftarm = nullptr;
+	Entity* rightarm = nullptr;
+	int bodypart = 0;
+	int limbSpeedMultiplier = 1;
 
 	// set invisibility //TODO: use isInvisible()?
 	if ( multiplayer != CLIENT )
@@ -268,7 +277,7 @@ void cockatriceMoveBodyparts(Entity* my, Stat* myStats, double dist)
 			my->flags[INVISIBLE] = true;
 			my->flags[BLOCKSIGHT] = false;
 			bodypart = 0;
-			for (node = my->children.first; node != NULL; node = node->next)
+			for (node = my->children.first; node != nullptr; node = node->next)
 			{
 				if ( bodypart < 2 )
 				{
@@ -293,7 +302,7 @@ void cockatriceMoveBodyparts(Entity* my, Stat* myStats, double dist)
 			my->flags[INVISIBLE] = false;
 			my->flags[BLOCKSIGHT] = true;
 			bodypart = 0;
-			for (node = my->children.first; node != NULL; node = node->next)
+			for (node = my->children.first; node != nullptr; node = node->next)
 			{
 				if ( bodypart < 2 )
 				{
@@ -321,7 +330,10 @@ void cockatriceMoveBodyparts(Entity* my, Stat* myStats, double dist)
 		}
 		else
 		{
-			my->pitch = 0;
+			if ( MONSTER_ATTACK != MONSTER_POSE_MAGIC_WINDUP2 && MONSTER_ATTACK != MONSTER_POSE_MELEE_WINDUP3 && my->monsterAnimationLimbOvershoot == ANIMATE_OVERSHOOT_NONE )
+			{
+				my->pitch = 0; // dont adjust head when attacking
+			}
 		}
 
 		// cockatrices are always flying
@@ -330,10 +342,28 @@ void cockatriceMoveBodyparts(Entity* my, Stat* myStats, double dist)
 	}
 
 	//Move bodyparts
-	for (bodypart = 0, node = my->children.first; node != NULL; node = node->next, bodypart++)
+	for (bodypart = 0, node = my->children.first; node != nullptr; node = node->next, bodypart++)
 	{
 		if ( bodypart < 2 )
 		{
+			if ( bodypart == 1 && multiplayer != CLIENT ) // only trigger once per loop, skip bodypart 0
+			{
+				if ( MONSTER_ATTACK == MONSTER_POSE_MAGIC_WINDUP2 )
+				{
+					if ( my->monsterAnimationLimbOvershoot >= ANIMATE_OVERSHOOT_TO_SETPOINT )
+					{
+						// handle z movement on windup
+						limbAnimateWithOvershoot(my, ANIMATE_Z, 0.2, -3.5, 0.05, -5.5, ANIMATE_DIR_POSITIVE); // default z is -4.5 in actmonster.cpp
+					}
+				}
+				else if(MONSTER_ATTACK != MONSTER_POSE_MELEE_WINDUP3 )
+				{
+					// post-swing head animation. client doesn't need to adjust the entity pitch, server will handle.
+					limbAnimateWithOvershoot(my, ANIMATE_PITCH, 0.2, PI / 4, 0.1, 0, ANIMATE_DIR_POSITIVE);
+					limbAnimateToLimit(my, ANIMATE_Z, 0.2, -4.5, false, 0);
+				}
+			}
+			
 			continue;
 		}
 		entity = (Entity*)node->element;
@@ -343,19 +373,30 @@ void cockatriceMoveBodyparts(Entity* my, Stat* myStats, double dist)
 		entity->yaw = my->yaw;
 		if ( bodypart == 3 || bodypart == 6 )
 		{
+			// right leg, left arm.
 			if ( bodypart == 3 )
 			{
-				rightbody = (Entity*)node->next->element;
+				// set leftbody to the left leg.
+				leftbody = (Entity*)node->next->element;
 			}
 			if ( bodypart == 3 || !MONSTER_ATTACK )
 			{
-				if ( !rightbody->skill[0] )
+				if ( (MONSTER_ATTACK == MONSTER_POSE_MAGIC_WINDUP2 || MONSTER_ATTACK == MONSTER_POSE_MELEE_WINDUP3) && bodypart == 3 )
 				{
-					entity->pitch -= COCKATRICEWALKSPEED;
+					limbSpeedMultiplier = 4;
+				}
+				else
+				{
+					limbSpeedMultiplier = 1;
+				}
+
+				if ( !leftbody->skill[0] )
+				{
+					entity->pitch -= COCKATRICEWALKSPEED * limbSpeedMultiplier;
 					if ( entity->pitch < -PI / 8.0 )
 					{
 						entity->pitch = -PI / 8.0;
-						if (bodypart == 3)
+						if ( bodypart == 3 )
 						{
 							entity->skill[0] = 1;
 						}
@@ -363,11 +404,11 @@ void cockatriceMoveBodyparts(Entity* my, Stat* myStats, double dist)
 				}
 				else
 				{
-					entity->pitch += COCKATRICEWALKSPEED;
+					entity->pitch += COCKATRICEWALKSPEED * limbSpeedMultiplier;
 					if ( entity->pitch > PI / 8.0 )
 					{
 						entity->pitch = PI / 8.0;
-						if (bodypart == 3)
+						if ( bodypart == 3 )
 						{
 							entity->skill[0] = 0;
 						}
@@ -376,77 +417,338 @@ void cockatriceMoveBodyparts(Entity* my, Stat* myStats, double dist)
 			}
 			else
 			{
-				// vertical chop
-				if ( MONSTER_ATTACKTIME == 0 )
+				rightarm = (Entity*)node->prev->element;
+				// ATTACK!
+				// move left arm
+
+				// vertical chop windup
+				if ( MONSTER_ATTACK == MONSTER_POSE_MELEE_WINDUP1 )
 				{
-					MONSTER_ARMBENDED = 0;
-					MONSTER_WEAPONYAW = 0;
-					entity->pitch = -3 * PI / 4;
-					entity->roll = 0;
-				}
-				else
-				{
-					if ( entity->pitch >= -PI / 2 )
+					if ( MONSTER_ATTACKTIME == 0 )
 					{
-						MONSTER_ARMBENDED = 1;
-					}
-					if ( entity->pitch >= PI / 4 )
-					{
-						entity->skill[0] = rightbody->skill[0];
-						MONSTER_WEAPONYAW = 0;
-						entity->pitch = rightbody->pitch;
+						// init rotations
+						entity->pitch = 0;
 						entity->roll = 0;
-						MONSTER_ARMBENDED = 0;
-						MONSTER_ATTACK = 0;
+					}
+
+					limbAnimateToLimit(entity, ANIMATE_PITCH, -0.25, 5 * PI / 4, false, 0);
+					entity->skill[0] = 0;
+
+					if ( MONSTER_ATTACKTIME >= ANIMATE_DURATION_WINDUP )
+					{
+						if ( multiplayer != CLIENT )
+						{
+							my->attack(1, 0, nullptr);
+						}
+					}
+				}
+				// vertical chop attack
+				else if ( MONSTER_ATTACK == 1 )
+				{
+					if ( MONSTER_ATTACKTIME > 0 )
+					{
+						if ( limbAnimateToLimit(entity, ANIMATE_PITCH, 0.3, PI / 3, false, 0.0) == 1 )
+						{
+							entity->skill[0] = leftbody->skill[0];
+							entity->pitch = leftbody->pitch;
+							MONSTER_ATTACK = 0;
+						}
+					}
+				}
+				// horizontal chop windup
+				if ( MONSTER_ATTACK == MONSTER_POSE_MELEE_WINDUP2 )
+				{
+					if ( MONSTER_ATTACKTIME == 0 )
+					{
+						// init rotations
+						entity->pitch = 0;
+						entity->roll = 0;
+					}
+
+					// get rightarm from bodypart 5 element if ready to attack
+					// vertical chop
+					if ( rightarm->pitch > PI && rightarm->pitch < 7 * PI / 4 )
+					{
+						limbAnimateToLimit(entity, ANIMATE_PITCH, -0.25, 6 * PI / 4, false, 0);
+						entity->skill[0] = 0;
+					}
+				}
+				// horizontal chop attack
+				else if ( MONSTER_ATTACK == 2 )
+				{
+					//limbAnimateToLimit(entity, ANIMATE_PITCH, 0.3, 0, false, 0.0);
+					if ( MONSTER_ATTACKTIME > 0 )
+					{
+						if ( entity->skill[0] == 0 )
+						{
+							if ( limbAnimateToLimit(entity, ANIMATE_PITCH, 0.25, PI / 3, false, 0) )
+							{
+								entity->skill[0] = 1;
+							}
+						}
+						else if ( entity->skill[0] == 1 )
+						{
+							entity->pitch = rightarm->pitch;
+						}
+					}
+				}
+				// double attack windup
+				else if ( MONSTER_ATTACK == MONSTER_POSE_MELEE_WINDUP3 )
+				{
+					if ( MONSTER_ATTACKTIME == 0 )
+					{
+						// init rotations
+						entity->pitch = 0;
+						entity->roll = 0;
+						playSoundEntityLocal(my, 79, 128);
+						if ( multiplayer != CLIENT )
+						{
+							createParticleDot(my);
+							serverSpawnMiscParticles(my, PARTICLE_EFFECT_ABILITY_PURPLE);
+							// cockatrice can't be paralyzed, use EFF_STUNNED instead.
+							myStats->EFFECTS[EFF_STUNNED] = true;
+							myStats->EFFECTS_TIMERS[EFF_STUNNED] = 20;
+						}
+						entity->skill[0] = 0;
+					}
+					else if ( MONSTER_ATTACKTIME > 20 )
+					{
+						entity->skill[0] = 2;
+						//entity->roll = 0;
 					}
 					else
 					{
-						entity->pitch += .25;
+						//limbAnimateToLimit(entity, ANIMATE_ROLL, 0.1, 1 * PI / 16, false, 0);
+					}
+
+					switch ( entity->skill[0] )
+					{
+						case 0:
+							// swing forwards.
+							if ( limbAnimateToLimit(entity, ANIMATE_PITCH, -0.25, 7 * PI / 4, false, 0) == 1 )
+							{
+								entity->skill[0] = 1;
+							}
+							break;
+						case 1:
+							// move backwards, shake at endpoint
+							// move the head.
+							limbAnimateToLimit(my, ANIMATE_PITCH, -0.1, 11 * PI / 6, true, 0.05);
+							limbAnimateToLimit(entity, ANIMATE_PITCH, 0.1, PI / 8, true, 0.05);
+							break;
+						case 2:
+							// start attack swing
+							if ( limbAnimateToLimit(entity, ANIMATE_PITCH, -0.4, 5 * PI / 4, false, 0) == 1 )
+							{
+								entity->skill[0] = 0;
+								//playSoundEntityLocal(my, 79, 128);
+								if ( multiplayer != CLIENT )
+								{
+									my->monsterAnimationLimbOvershoot = ANIMATE_OVERSHOOT_TO_SETPOINT;
+									my->attack(MONSTER_POSE_COCKATRICE_DOUBLEATTACK, 0, nullptr);
+								}
+							}
+							break;
+						default:
+							break;
+					}
+				}
+				// double attack
+				else if ( MONSTER_ATTACK == MONSTER_POSE_COCKATRICE_DOUBLEATTACK )
+				{
+					switch ( entity->skill[0] )
+					{
+						case 0:
+							// swing down
+							entity->roll = 31 * PI / 16;
+							if ( limbAnimateToLimit(entity, ANIMATE_PITCH, 0.5, PI / 3, false, 0.0) == 1 )
+							{
+								entity->skill[0] = 1;
+							}
+							break;
+						case 1:
+							// raise arms up again
+							entity->roll = 0;
+							if ( limbAnimateToLimit(entity, ANIMATE_PITCH, -0.4, 5 * PI / 4, false, 0) == 1 )
+							{
+								entity->roll = 31 * PI / 16;
+								entity->skill[0] = 2;
+								if ( multiplayer != CLIENT )
+								{
+									my->attack(MONSTER_POSE_COCKATRICE_DOUBLEATTACK, 0, nullptr);
+								}
+							}
+							break;
+						case 2:
+							// swing down and end.						
+							if ( limbAnimateToLimit(entity, ANIMATE_PITCH, 0.5, PI / 3, false, 0.0) == 1 )
+							{
+								entity->roll = 0;
+								entity->skill[0] = leftbody->skill[0];
+								entity->pitch = leftbody->pitch;
+								MONSTER_ATTACK = 0;
+							}
+							break;
+						default:
+							break;
+					}
+				}
+				else if ( MONSTER_ATTACK == MONSTER_POSE_MAGIC_WINDUP2 )
+				{
+					if ( MONSTER_ATTACKTIME == 0 )
+					{
+						// init rotations
+						entity->pitch = 0;
+						entity->roll = 0;
+						//my->monsterAnimationLimbOvershoot = ANIMATE_OVERSHOOT_TO_SETPOINT;
+						playSoundEntityLocal(my, 79, 128);
+						if ( multiplayer != CLIENT )
+						{
+							createParticleDot(my);
+							serverSpawnMiscParticles(my, PARTICLE_EFFECT_ABILITY_PURPLE);
+							// cockatrice can't be paralyzed, use EFF_STUNNED instead.
+							myStats->EFFECTS[EFF_STUNNED] = true;
+							myStats->EFFECTS_TIMERS[EFF_STUNNED] = 50;
+						}
+					}
+
+					if ( my->monsterAnimationLimbOvershoot != ANIMATE_OVERSHOOT_TO_SETPOINT )
+					{
+						// move the head.
+						limbAnimateToLimit(my, ANIMATE_PITCH, -0.1, 11 * PI / 6, true, 0.05);
+
+						// raise left arm and tilt.
+						limbAnimateToLimit(entity, ANIMATE_ROLL, -0.1, 31 * PI / 16, false, 0);
+						limbAnimateToLimit(entity, ANIMATE_PITCH, -0.1, 5 * PI / 4, false, 0);
+					}
+
+					if ( MONSTER_ATTACKTIME > 50 )
+					{
+						// reset roll
+						entity->roll = 0;
+
+						if ( multiplayer != CLIENT )
+						{
+							// set overshoot for head animation
+							my->monsterAnimationLimbOvershoot = ANIMATE_OVERSHOOT_TO_SETPOINT;
+							my->attack(3, 0, nullptr);
+						}
+					}
+
+					MONSTER_ATTACKTIME++; // manually increment counter
+				}
+				// default swing
+				else if ( MONSTER_ATTACK == 3 )
+				{
+					if ( limbAnimateToLimit(entity, ANIMATE_PITCH, 0.3, PI / 3, false, 0.0) == 1 )
+					{
+						entity->skill[0] = leftbody->skill[0];
+						entity->pitch = leftbody->pitch;
+						MONSTER_ATTACK = 0;
 					}
 				}
 			}
 		}
 		else if ( bodypart == 4 || bodypart == 5 )
 		{
+			// right arm
 			if ( bodypart == 5 )
 			{
-				if ( MONSTER_ATTACK )
+				if ( MONSTER_ATTACK > 0 )
 				{
+					// get leftarm from bodypart 6 element if ready to attack
+					leftarm = (Entity*)node->next->element;
 					// vertical chop
-					if ( MONSTER_ATTACKTIME == 0 )
+					if ( MONSTER_ATTACK == MONSTER_POSE_MELEE_WINDUP1 )
 					{
-						MONSTER_ARMBENDED = 0;
-						MONSTER_WEAPONYAW = 0;
-						entity->pitch = -3 * PI / 4;
-						entity->roll = 0;
+						if ( MONSTER_ATTACKTIME == 0 )
+						{
+							// init rotations.
+							entity->pitch = 0;
+							entity->roll = 0;
+						}
+						else if ( leftarm->pitch > PI && leftarm->pitch < 7 * PI / 4 )
+						{
+							limbAnimateToLimit(entity, ANIMATE_PITCH, -0.25, 5 * PI / 4, false, 0);
+							entity->skill[0] = 0;
+						}
+					}
+					// vertical chop attack
+					else if ( MONSTER_ATTACK == 1 )
+					{
+						if ( MONSTER_ATTACKTIME > 0 )
+						{
+							if ( entity->skill[0] == 0 )
+							{
+								if ( limbAnimateToLimit(entity, ANIMATE_PITCH, 0.3, PI / 3, false, 0) )
+								{
+									entity->skill[0] = 1;
+								}
+							}
+							else if ( entity->skill[0] == 1 )
+							{
+								entity->pitch = leftarm->pitch;
+							}
+						}
+					}
+
+					else if ( MONSTER_ATTACK == MONSTER_POSE_MELEE_WINDUP2 )
+					{
+						if ( MONSTER_ATTACKTIME == 0 )
+						{
+							// init rotations
+							entity->pitch = 0;
+							entity->roll = 0;
+						}
+						limbAnimateToLimit(entity, ANIMATE_PITCH, -0.25, 6 * PI / 4, false, 0);
+						limbAnimateToLimit(entity, ANIMATE_ROLL, -0.1, 31 * PI / 16, false, 0);
+						entity->monsterAnimationLimbOvershoot = ANIMATE_OVERSHOOT_TO_SETPOINT;
+
+						if ( MONSTER_ATTACKTIME >= ANIMATE_DURATION_WINDUP )
+						{
+							if ( multiplayer != CLIENT )
+							{
+								my->attack(2, 0, nullptr);
+							}
+						}
+					}
+					else if ( MONSTER_ATTACK == 2 )
+					{
+						if ( MONSTER_ATTACKTIME > 0 )
+						{
+							limbAnimateToLimit(entity, ANIMATE_PITCH, 0.3, 0, false, 0.0);
+							if ( limbAnimateWithOvershoot(entity, ANIMATE_ROLL, 0.1, PI / 8, 0.05, 0, ANIMATE_DIR_POSITIVE) == ANIMATE_OVERSHOOT_TO_ENDPOINT )
+							{
+								entity->skill[0] = leftbody->skill[0];
+								entity->pitch = leftbody->pitch;
+								entity->roll = 0;
+								MONSTER_ATTACK = 0;
+							}
+						}
 					}
 					else
 					{
-						if ( entity->pitch >= -PI / 2 )
+						if ( leftarm != nullptr )
 						{
-							MONSTER_ARMBENDED = 1;
-						}
-						if ( entity->pitch >= PI / 4 )
-						{
-							entity->skill[0] = rightbody->skill[0];
-							MONSTER_WEAPONYAW = 0;
-							entity->pitch = rightbody->pitch;
-							entity->roll = 0;
-							MONSTER_ARMBENDED = 0;
-						}
-						else
-						{
-							entity->pitch += .25;
+							// follow the left arm animation.
+							entity->pitch = leftarm->pitch;
+							entity->roll = -leftarm->roll;
 						}
 					}
 				}
+				else
+				{
+					entity->skill[0] = leftbody->skill[0];
+					entity->pitch = leftbody->pitch;
+					entity->roll = 0;
+				}
 			}
-
+			// swing right arm/ left leg in sync
 			if ( bodypart != 5 || (MONSTER_ATTACK == 0 && MONSTER_ATTACKTIME == 0) )
 			{
 				if ( entity->skill[0] )
 				{
-					entity->pitch -= COCKATRICEWALKSPEED;
+					entity->pitch -= COCKATRICEWALKSPEED  * limbSpeedMultiplier;
 					if ( entity->pitch < -PI / 8.0 )
 					{
 						entity->skill[0] = 0;
@@ -455,7 +757,7 @@ void cockatriceMoveBodyparts(Entity* my, Stat* myStats, double dist)
 				}
 				else
 				{
-					entity->pitch += COCKATRICEWALKSPEED;
+					entity->pitch += COCKATRICEWALKSPEED  * limbSpeedMultiplier;
 					if ( entity->pitch > PI / 8.0 )
 					{
 						entity->skill[0] = 1;
@@ -464,9 +766,18 @@ void cockatriceMoveBodyparts(Entity* my, Stat* myStats, double dist)
 				}
 			}
 		}
+		// wings
 		else if ( bodypart == 7 || bodypart == 8 )
 		{
-			entity->fskill[1] += .1;
+			if ( MONSTER_ATTACK == MONSTER_POSE_MAGIC_WINDUP2 || MONSTER_ATTACK == MONSTER_POSE_MELEE_WINDUP3 )
+			{
+				// flap wings faster during attack.
+				entity->fskill[1] += .4;
+			}
+			else
+			{
+				entity->fskill[1] += .1;
+			}
 			if ( entity->fskill[1] >= PI * 2 )
 			{
 				entity->fskill[1] -= PI * 2;
@@ -521,12 +832,16 @@ void cockatriceMoveBodyparts(Entity* my, Stat* myStats, double dist)
 				break;
 		}
 	}
-	if ( MONSTER_ATTACK != 0 )
+	if ( MONSTER_ATTACK > 0 && MONSTER_ATTACK <= MONSTER_POSE_MELEE_WINDUP3 )
 	{
 		MONSTER_ATTACKTIME++;
 	}
-	else
+	else if ( MONSTER_ATTACK == 0 )
 	{
 		MONSTER_ATTACKTIME = 0;
+	}
+	else
+	{
+		// do nothing, don't reset attacktime or increment it.
 	}
 }
