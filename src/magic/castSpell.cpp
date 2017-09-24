bff98f5a79aad23ff838d020c1817d63130ece56
WALL OF JUSTICE <->
2017-09-24 20:17:56 +1000

    + magic particle effects "sap" and "erupt", called with createParticleSapCenter and createParticleDropRising * adjust summon spell to debug these fx. + PARTICLE_EFFECT_SHADOW_INVIS constant to call black particles for dropRising. * Verified working consistently with server/client in multiplayer.

--- src/magic/castSpell.cpp
+++ src/magic/castSpell.cpp
@@ -715,9 +715,14 @@ Entity* castSpell(Uint32 caster_uid, spell_t* spell, bool using_magicstaff, bool
 				if ( caster == players[i]->entity )
 				{
 					//spawnMagicEffectParticles(caster->x, caster->y, caster->z, 171);
-					createParticle1(caster, i);
+					//createParticle1(caster, i);
+					//createParticleDropRising(caster);
 				}
 			}
+			createParticleDropRising(caster, 593);
+			serverSpawnMiscParticles(caster, PARTICLE_EFFECT_SHADOW_INVIS);
+
+			//createParticleSapCenter(caster, caster->x + 64 * cos(caster->yaw), caster->y + 64 * sin(caster->yaw), 172, 172);
 			playSoundEntity(caster, 167, 128);
 		}
 		else if ( !strcmp(element->name, spellElement_reflectMagic.name) )
