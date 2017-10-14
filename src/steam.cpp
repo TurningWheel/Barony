32f773a1824075e7c5f29591e9714b031fef979d
Lutz <kellenc0105@my.uwstout.edu>
2017-10-13 20:39:18 -0500

    * Removed the '!' before the cheats enabled check

--- src/steam.cpp
+++ src/steam.cpp
@@ -613,7 +613,7 @@ void steamAchievement(const char* achName)
 	return;
 #else
 
-	if ( !(svFlags & SV_FLAG_CHEATS) )
+	if ( svFlags & SV_FLAG_CHEATS )
 	{
 		// Cheats are enabled, therefore you cannot earn Steam Achievements
 		return;
@@ -651,7 +651,7 @@ void steamAchievementClient(int player, const char* achName)
 		return;
 	}
 
-	if ( !(svFlags & SV_FLAG_CHEATS) )
+	if ( svFlags & SV_FLAG_CHEATS )
 	{
 		// Cheats are enabled, therefore you cannot earn Steam Achievements. This check prevents needless packet sending
 		return;
