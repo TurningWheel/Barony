df5eb7a4bc1394c76e3077e815ca072b24133595
WALL OF JUSTICE <->
2017-11-13 22:14:19 +1100

    + arrowPower(skill[3]),  arrowPoisonTime(skill[4]),  arrowArmorPierce(skill[5]) * Change ranged weapon attack to be based off DEX, not STR. + Chance to pierce 50% of targets armor, depending on a PER roll. + On hit message for armor pierce.

--- src/entity.hpp
+++ src/entity.hpp
@@ -288,6 +288,11 @@ public:
 	Sint32& pistonCamTimer;
 	real_t& pistonCamRotateSpeed;
 
+	//--PUBLIC ARROR/PROJECTILE SKILLS--
+	Sint32& arrowPower;
+	Sint32& arrowPoisonTime;
+	Sint32& arrowArmorPierce;
+
 	void pedestalOrbInit(); // init orb properties
 
 	// a pointer to the entity's location in a list (ie the map list of entities)
@@ -507,6 +512,8 @@ public:
 	bool backupWithRangedWeapon(Stat& myStats, int dist, int hasrangedweapon);
 	// calc time required for a mana regen tick.
 	int getManaRegenInterval(Stat& myStats); 
+	// calc damage/effects for ranged weapons.
+	void setRangedProjectileAttack(Entity& marksman, Stat& myStats);
 
 	spell_t* getActiveMagicEffect(int spellID);
 
