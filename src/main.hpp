e373b6e0ccbb0ed673b899151c0c73d6507dd514
Ciprian <ciliesj@gmail.com>
2017-11-26 03:09:14 -0800

    * Added parantheses guard around MFLAG_* #defines.

--- src/main.hpp
+++ src/main.hpp
@@ -295,9 +295,9 @@ static const int MAP_FLAG_DISABLELEVITATION = 14;
 static const int MAP_FLAG_GENADJACENTROOMS = 15;
 
 #define MFLAG_DISABLEDIGGING (map.flags[MAP_FLAG_GENBYTES3] >> 24) & 0xFF // first leftmost byte
-#define MFLAG_DISABLETELEPORT (map.flags[MAP_FLAG_GENBYTES3] >> 16) & 0xFF // second leftmost byte
-#define MFLAG_DISABLELEVITATION (map.flags[MAP_FLAG_GENBYTES3] >> 8) & 0xFF // third leftmost byte
-#define MFLAG_GENADJACENTROOMS (map.flags[MAP_FLAG_GENBYTES3] >> 0) & 0xFF // fourth leftmost byte
+#define MFLAG_DISABLETELEPORT ((map.flags[MAP_FLAG_GENBYTES3] >> 16) & 0xFF) // second leftmost byte
+#define MFLAG_DISABLELEVITATION ((map.flags[MAP_FLAG_GENBYTES3] >> 8) & 0xFF) // third leftmost byte
+#define MFLAG_GENADJACENTROOMS ((map.flags[MAP_FLAG_GENBYTES3] >> 0) & 0xFF) // fourth leftmost byte
 
 // light structure
 typedef struct light_t
