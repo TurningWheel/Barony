4562b918f7f9e3258d90be7042cc4ab31aff017c
WALL OF JUSTICE <->
2017-12-17 15:17:54 +1100

    * Slight adjust labyrinth

--- src/maps.cpp
+++ src/maps.cpp
@@ -106,18 +106,18 @@ int monsterCurve(int level)
 			case 6:
 			case 7:
 			case 8:
-			case 9:
 				return SCORPION;
+			case 9:
 			case 10:
 			case 11:
 			case 12:
-			case 13:
 				return TROLL;
+			case 13:
 			case 14:
 			case 15:
 			case 16:
-			case 17:
 				return SCARAB;
+			case 17:
 			case 18:
 			case 19:
 				return INSECTOID;
