2b3c687e11a8cfd2720b8e3b58e8bc42aeb1e17c
Linus Heckemann <git@sphalerite.org>
2018-01-05 11:57:48 +0000

    Include short git hash in version string
    This can make tracing issues back to the code a lot easier, especially
    with steam versions, for people who do not know what happens to be the
    current version on Steam etc., see #312
    
    Also fix the version strings for the OSX bundle while we're at it. There
    should only be one source of truth for the version number now.

--- src/game.hpp
+++ src/game.hpp
@@ -14,8 +14,6 @@
 #include <vector>
 #include <random>
 
-// REMEMBER TO CHANGE THIS WITH EVERY NEW OFFICIAL VERSION!!!
-#define VERSION "v3.0.1"
 #define GAME_CODE
 
 //#define MAX_FPS_LIMIT 60 //TODO: Make this configurable.
