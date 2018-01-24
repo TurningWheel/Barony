8818b172a88a76d41dd6640308dbd4ffbba9770b
WALL OF JUSTICE <->
2018-01-24 20:23:51 +1100

    + Add exclusion for magic particles in pathing

--- src/paths.cpp
+++ src/paths.cpp
@@ -246,7 +246,7 @@ list_t* generatePath(int x1, int y1, int x2, int y2, Entity* my, Entity* target,
 		{
 			continue;
 		}
-		if ( entity->behavior == &actDoorFrame || entity->behavior == &actDoor )
+		if ( entity->behavior == &actDoorFrame || entity->behavior == &actDoor || entity->behavior == &actMagic )
 		{
 			continue;
 		}
