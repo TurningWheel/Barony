2c8d9076b2511a04121047f2bfd40507bb8efe03
WALL OF JUSTICE <->
2018-02-08 19:52:19 +1100

    * Add staffs with the right appearance to the undroppable monster table.

--- src/items.cpp
+++ src/items.cpp
@@ -1134,7 +1134,7 @@ Entity* dropItemMonster(Item* item, Entity* monster, Stat* monsterStats, Sint16 
 				|| monsterStats->type == INSECTOID
 				|| monsterStats->type == INCUBUS
 				|| monsterStats->type == VAMPIRE)
-				&& itemCategory(item) == SPELLBOOK )
+				&& (itemCategory(item) == SPELLBOOK || itemCategory(item) == MAGICSTAFF) )
 			{
 				// monsters with special spell attacks won't drop their book.
 				itemDroppable = false;
