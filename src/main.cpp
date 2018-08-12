5e83aac7ea2a5355f35d292a34061085bfb5e618
WALL OF JUSTICE <->
2018-08-12 13:05:29 +1000

    * Fix out of bounds in mapflagtext[] accessed when using DISABLE_OPENING 16 index.

--- src/main.cpp
+++ src/main.cpp
@@ -185,7 +185,7 @@ char oldfilename[128];
 char message[256];
 int messagetime = 0;
 char widthtext[4], heighttext[4], nametext[32], authortext[32], skyboxtext[32];
-char mapflagtext[MAPFLAGS][32];
+char mapflagtext[MAPFLAGTEXTS][32];
 char spriteProperties[32][128];
 char tmpSpriteProperties[32][128];
 int editproperty = 0;
