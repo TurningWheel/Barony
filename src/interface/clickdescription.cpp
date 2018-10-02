7ef54e1d8ace33733bc7410f691415ea01224f4b
WALL OF JUSTICE <->
2018-10-02 23:27:48 +1000

    * fix click description on non-human races for players

--- src/interface/clickdescription.cpp
+++ src/interface/clickdescription.cpp
@@ -317,7 +317,18 @@ void clickDescription(int player, Entity* entity)
 			}
 			else
 			{
-				messagePlayerMonsterEvent(player, 0xFFFFFFFF, *stat, language[254], language[253], MSG_DESCRIPTION, entity);
+				if ( entity->behavior == &actPlayerLimb )
+				{
+					Entity* parent = uidToEntity(entity->parent);
+					if ( parent )
+					{
+						messagePlayerMonsterEvent(player, 0xFFFFFFFF, *stat, language[254], language[253], MSG_DESCRIPTION, parent);
+					}
+				}
+				else
+				{
+					messagePlayerMonsterEvent(player, 0xFFFFFFFF, *stat, language[254], language[253], MSG_DESCRIPTION, entity);
+				}
 			}
 		}
 		else
