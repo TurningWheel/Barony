742d73e102bf554e1a64fe829be6d05a495afddc
WALL OF JUSTICE <->
2018-11-16 09:56:14 +1100

    * Fix 'use previous character' button overwriting the oldest save files always. also fix connecting to a steam lobby via invite and pressing this button

--- src/menu.cpp
+++ src/menu.cpp
@@ -11186,7 +11186,6 @@ void buttonReplayLastCharacter(button_t* my)
 	if ( lastCreatedCharacterClass >= 0 )
 	{
 		playing_random_char = false;
-		charcreation_step = 5;
 		camera_charsheet_offsetyaw = (330) * PI / 180;
 		stats[0]->sex = static_cast<sex_t>(lastCreatedCharacterSex);
 		client_classes[0] = lastCreatedCharacterClass;
@@ -11194,6 +11193,8 @@ void buttonReplayLastCharacter(button_t* my)
 		initClass(0);
 		stats[0]->appearance = lastCreatedCharacterAppearance;
 		strcpy(stats[0]->name, lastname.c_str());
+		charcreation_step = 4; // set the step to 4, so clicking continue advances to 5 (single/multiplayer select)
+		buttonContinue(nullptr);
 	}
 }
 
