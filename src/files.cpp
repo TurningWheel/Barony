fa23d6c7556462e98b1d2885e00a5172adb252c1
WALL OF JUSTICE <->
2018-12-09 18:37:41 +1100

    * cockatrice lair no levitate due to possible stuck scenario with levitation potion

--- src/files.cpp
+++ src/files.cpp
@@ -298,7 +298,7 @@ std::unordered_map<std::string, int> mapHashes =
 	{ "caves24d.lmp", 391561 },
 	{ "cavestocitadel.lmp", 215724 },
 	{ "cavessecret.lmp", 198959 },
-	{ "caveslair.lmp", 4872675 },
+	{ "caveslair.lmp", 4625772 },
 	{ "citadel.lmp", 729069 },
 	{ "citadel00.lmp", 23997 },
 	{ "citadel01.lmp", 30094 },
