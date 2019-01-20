d52a7fa7315605a299151701b7488d7946541112
WALL OF JUSTICE <->
2019-01-20 16:50:44 +1100

    * bump lang entries up to 3400

--- src/main.hpp
+++ src/main.hpp
@@ -506,7 +506,7 @@ extern int minotaurlevel;
 #define DIRECTCLIENT 4
 
 // language stuff
-#define NUMLANGENTRIES 3350
+#define NUMLANGENTRIES 3400
 extern char languageCode[32];
 extern char** language;
 
