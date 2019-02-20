a87849b17b6d61b2860d30c715e26d78fef1034b
WALL OF JUSTICE <->
2019-02-21 02:42:34 +1100

    * holding ALT while right clicking/hotbar using a potion force equips in hand

--- src/interface/playerinventory.cpp
+++ src/interface/playerinventory.cpp
@@ -1268,6 +1268,29 @@ void updatePlayerInventory()
 							//Cleanup identify GUI gamecontroller code here.
 							selectedIdentifySlot = -1;
 						}
+						else if ( itemCategory(item) == POTION && 
+							(keystatus[SDL_SCANCODE_LALT] || keystatus[SDL_SCANCODE_RALT]) 
+							&& !(*inputPressed(joyimpulses[INJOY_MENU_USE])) )
+						{
+							mousestatus[SDL_BUTTON_RIGHT] = 0;
+							// force equip potion
+							if ( multiplayer == CLIENT )
+							{
+								strcpy((char*)net_packet->data, "EQUI");
+								SDLNet_Write32((Uint32)item->type, &net_packet->data[4]);
+								SDLNet_Write32((Uint32)item->status, &net_packet->data[8]);
+								SDLNet_Write32((Uint32)item->beatitude, &net_packet->data[12]);
+								SDLNet_Write32((Uint32)item->count, &net_packet->data[16]);
+								SDLNet_Write32((Uint32)item->appearance, &net_packet->data[20]);
+								net_packet->data[24] = item->identified;
+								net_packet->data[25] = clientnum;
+								net_packet->address.host = net_server.host;
+								net_packet->address.port = net_server.port;
+								net_packet->len = 26;
+								sendPacketSafe(net_sock, -1, net_packet, 0);
+							}
+							equipItem(item, &stats[clientnum]->weapon, clientnum);
+						}
 						else
 						{
 							// open a drop-down menu of options for "using" the item
