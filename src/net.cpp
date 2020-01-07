4fd11ca981b75c8697b15d35b18b23e2aacb1c90
WALL OF JUSTICE <->
2020-01-07 17:05:01 +1100

    * limit dash speed possible

--- src/net.cpp
+++ src/net.cpp
@@ -3849,7 +3849,7 @@ void clientHandlePacket()
 		if ( players[clientnum] && players[clientnum]->entity && stats[clientnum] )
 		{
 			real_t vel = sqrt(pow(players[clientnum]->entity->vel_y, 2) + pow(players[clientnum]->entity->vel_x, 2));
-			players[clientnum]->entity->monsterKnockbackVelocity = std::max(1.0, vel);
+			players[clientnum]->entity->monsterKnockbackVelocity = std::min(2.25, std::max(1.0, vel));
 			players[clientnum]->entity->monsterKnockbackTangentDir = atan2(players[clientnum]->entity->vel_y, players[clientnum]->entity->vel_x);
 			if ( vel < 0.01 )
 			{
