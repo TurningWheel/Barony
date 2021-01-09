a3e3caab89deb3ab45cf1ef1ac5d716bcd490001
SheridanR <sheridan.rathbun@gmail.com>
2021-01-09 13:30:37 -0800

    A bit more HUD stuff
    Signed-off-by: SheridanR <sheridan.rathbun@gmail.com>
    
    # Conflicts:
    #	src/main.hpp

--- src/ui/GameUI.cpp
+++ src/ui/GameUI.cpp
@@ -5,17 +5,43 @@
 #include "Image.hpp"
 #include "Field.hpp"
 
+#include "../main.hpp"
+#include "../game.hpp"
+#include "../menu.hpp"
+#include "../interface/interface.hpp"
+
 #include <assert.h>
 
-static Frame* playerHud = nullptr;
+static Frame* playerHud[MAXPLAYERS] = { nullptr };
 
-void createIngameHud() {
-    Frame* frame = gui->addFrame("player hud");
-    playerHud = frame;
+void createIngameHud(int player) {
+    char name[32];
+    snprintf(name, sizeof(name), "player hud %d", player);
+    Frame* frame = gui->addFrame(name);
+
+    playerHud[player] = frame;
+    int playercount = 0;
+    for (int c = 0; c < MAXPLAYERS; ++c) {
+        if (!client_disconnected[c]) {
+            ++playercount;
+        }
+    }
+
+    static const char* bigfont = "fonts/pixelmix.ttf#18";
+    static const char* smallfont = "fonts/pixel_maz.ttf#32";
 
     // big empty frame to serve as the root
-    frame->setSize(SDL_Rect{0, 0, Frame::virtualScreenX, Frame::virtualScreenY});
-    frame->setActualSize(SDL_Rect{0, 0, Frame::virtualScreenX, Frame::virtualScreenY});
+    if (playercount == 1) {
+        frame->setSize(SDL_Rect{0, 0, Frame::virtualScreenX, Frame::virtualScreenY});
+    } else if (playercount == 2) {
+        int y = (player % 2) * Frame::virtualScreenY / 2;
+        frame->setSize(SDL_Rect{0, y, Frame::virtualScreenX, Frame::virtualScreenY / 2});
+    } else if (playercount >= 3 || playercount <= 4) {
+        int x = (player % 2) * Frame::virtualScreenX / 2;
+        int y = (player / 2) * Frame::virtualScreenY / 2;
+        frame->setSize(SDL_Rect{x, y, Frame::virtualScreenX / 2, Frame::virtualScreenY / 2});
+    }
+    frame->setActualSize(SDL_Rect{0, 0, frame->getSize().w, frame->getSize().h});
     frame->setHollow(true);
     frame->setBorder(0);
 
@@ -23,7 +49,7 @@ void createIngameHud() {
     Frame* chat = frame->addFrame("chat");
     chat->setSize(SDL_Rect{224, 16, 832, 200});
     chat->setActualSize(SDL_Rect{0, 0, 832, 200});
-    chat->setFont("fonts/pixelmix.ttf#18");
+    chat->setFont(bigfont);
     {
         auto e = chat->addEntry("chat", true);
         e->color = 0xff00ffff;
@@ -50,6 +76,99 @@ void createIngameHud() {
         e->text = "Player 2: Chatlog text is the best, I swear my life on it.";
     }
 
+    // other player statuses
+    for (int i = 0, c = 0; c < MAXPLAYERS; ++c) {
+        if (c == player) {
+            continue;
+        }
+
+        // player name
+        {
+            char name[32];
+            snprintf(name, sizeof(name), "Player %d Name", c + 1);
+            Field* field = frame->addField(name, sizeof(name));
+            Uint32 color = 0xffffffff;
+            switch (c) {
+            case 0: color = 0xffffffff; break;
+            case 1: color = 0xffff8800; break;
+            case 2: color = 0xff00ffff; break;
+            case 3: color = 0xffff00ff; break;
+            }
+            field->setSize(SDL_Rect{16, 8 + 56 * i, 320, 64});
+            field->setText(name);
+            field->setFont(smallfont);
+            field->setColor(color);
+        }
+
+        // hp bar
+        {
+            char name[32];
+            snprintf(name, sizeof(name), "hp %d", c + 1);
+            Frame* hp = frame->addFrame(name);
+            hp->setColor(0xffffffff);
+            hp->setBorderStyle(Frame::BORDER_BEVEL_HIGH);
+            hp->setBorder(2);
+            hp->setSize(SDL_Rect{16, 32 + 56 * i, 160, 16});
+            hp->setActualSize(SDL_Rect{0, 0, hp->getSize().w, hp->getSize().h});
+            auto red = hp->addImage(
+                SDL_Rect{4, 4, hp->getSize().w - 8, hp->getSize().h - 8},
+                0xff8888ff,
+                "images/system/white.png",
+                "red"
+            );
+        }
+
+        // mp bar
+        {
+            char name[32];
+            snprintf(name, sizeof(name), "mp %d", c + 1);
+            Frame* mp = frame->addFrame(name);
+            mp->setColor(0xffffffff);
+            mp->setBorderStyle(Frame::BORDER_BEVEL_HIGH);
+            mp->setBorder(2);
+            mp->setSize(SDL_Rect{16, 48 + 56 * i, 160, 16});
+            mp->setActualSize(SDL_Rect{0, 0, mp->getSize().w, mp->getSize().h});
+            auto blue = mp->addImage(
+                SDL_Rect{4, 4, mp->getSize().w - 8, mp->getSize().h - 8},
+                0xffff8888,
+                "images/system/white.png",
+                "blue"
+            );
+        }
+
+        // position
+        ++i;
+    }
+
+    // ally hp
+    for (int c = 0; c < 8; ++c) {
+        // text
+        {
+            char text[32];
+            snprintf(text, sizeof(text), "Ally %d", c + 1);
+            Field* field = frame->addField(text, sizeof(text));
+            field->setText(text);
+            field->setSize(SDL_Rect{frame->getSize().w - 176, 8 + 18 * c, 64, 24});
+            field->setFont(smallfont);
+            field->setColor(0xff888888);
+        }
+
+        // bar
+        {
+            char name[32];
+            snprintf(name, sizeof(name), "ally hp bar %d", c + 1);
+            Frame* bar = frame->addFrame(name);
+            bar->setSize(SDL_Rect{frame->getSize().w - 128, 14 + 18 * c, 112, 14});
+            bar->setActualSize(SDL_Rect{0, 0, bar->getSize().w, bar->getSize().h});
+            bar->setColor(0xffaaaaaa);
+            auto red = bar->addImage(
+                SDL_Rect{2, 2, bar->getSize().w - 4, bar->getSize().h - 4},
+                0xff4444dd,
+                "images/system/white.png",
+                "red"
+            );
+        }
+    }
 
     // hotbar
     static int num_hotbar_slots = 10;
@@ -120,7 +239,7 @@ void createIngameHud() {
             Field* text = xpbar->addField("text", 64);
             text->setSize(SDL_Rect{2, 4, xpbar->getSize().w - 4, xpbar->getSize().h - 4});
             text->setText("22 / 100 XP");
-            text->setFont("fonts/pixel_maz.ttf#32");
+            text->setFont(smallfont);
             text->setColor(0xff888888);
             text->setHJustify(Field::justify_t::CENTER);
             text->setVJustify(Field::justify_t::CENTER);
@@ -163,7 +282,7 @@ void createIngameHud() {
             Field* text = hp_bar->addField("text", 64);
             text->setSize(SDL_Rect{6, 3, 64, hp_bar->getSize().h - 4});
             text->setText("HP");
-            text->setFont("fonts/pixel_maz.ttf#32");
+            text->setFont(smallfont);
             text->setColor(0xffffffff);
             text->setHJustify(Field::justify_t::LEFT);
             text->setVJustify(Field::justify_t::CENTER);
@@ -198,7 +317,7 @@ void createIngameHud() {
             Field* text = mp_bar->addField("text", 64);
             text->setSize(SDL_Rect{6, 3, 64, mp_bar->getSize().h - 4});
             text->setText("MP");
-            text->setFont("fonts/pixel_maz.ttf#32");
+            text->setFont(smallfont);
             text->setColor(0xffffffff);
             text->setHJustify(Field::justify_t::LEFT);
             text->setVJustify(Field::justify_t::CENTER);
@@ -215,7 +334,13 @@ void createIngameHud() {
 }
 
 void newIngameHud() {
-    if (!playerHud) {
-        createIngameHud();
+    if (!nohud) {
+        // here is where splitscreen
+        if (!playerHud[clientnum]) {
+            createIngameHud(clientnum);
+        }
+
+        // this already works fine, so just reuse it
+        drawMinimap();
     }
 }
