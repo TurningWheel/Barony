ce9b618f89535b39a30d6ea0183eeb2f54b5d454
Sylvain <sylvain.becker@gmail.com>
2021-04-11 17:06:48 +0200

    Make sure the real SDL_Surface 'pitch' is taken into account when loading an image

--- src/files.cpp
+++ src/files.cpp
@@ -484,6 +484,7 @@ void glLoadTexture(SDL_Surface* image, int texnum)
 	glEnable(GL_TEXTURE_2D);
 	glBindTexture(GL_TEXTURE_2D, texid[texnum]);
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glPixelStorei(GL_UNPACK_ROW_LENGTH, (image->pitch / 4));
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
