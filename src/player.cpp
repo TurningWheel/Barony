24e57327ab6a48b43e7b15551a652632cb18b2e5
SheridanR <sheridan.rathbun@gmail.com>
2022-03-02 21:36:09 -0800

    isLocalPlayer() shenanigans

--- src/player.cpp
+++ src/player.cpp
@@ -2330,7 +2330,7 @@ void Player::cleanUpOnEntityRemoval()
 
 const bool Player::isLocalPlayer() const
 {
-	return ((splitscreen && bSplitscreen) || playernum == clientnum);
+	return ((splitscreen && bSplitscreen) || playernum == clientnum || intro);
 }
 const bool Player::isLocalPlayerAlive() const
 {
