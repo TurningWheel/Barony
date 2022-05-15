706cf7f4261e39408887377ec652231a54b54d53
WALL OF JUSTICE <->
2022-05-16 02:10:22 +1000

    * bump version number to 3.8.1 * fix bug in getSaveGameInfo missing data reads * add alchemy recipes to savegame data

--- src/game.hpp
+++ src/game.hpp
@@ -21,7 +21,7 @@
 #endif
 
 // REMEMBER TO CHANGE THIS WITH EVERY NEW OFFICIAL VERSION!!!
-#define VERSION "v3.8.0"
+#define VERSION "v3.8.1"
 #define GAME_CODE
 
 //#define MAX_FPS_LIMIT 60 //TODO: Make this configurable.
