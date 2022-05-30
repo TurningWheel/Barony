ab23ddfa0f1ac066f925e27a33e3776fb56f80c6
WALL OF JUSTICE <->
2022-05-31 01:33:28 +1000

    * editor compile fix

--- src/opengl.cpp
+++ src/opengl.cpp
@@ -1169,6 +1169,7 @@ bool glDrawEnemyBarSprite(view_t* camera, int mode, void* enemyHPBarDetails, boo
 
 void glDrawWorldUISprite(view_t* camera, Entity* entity, int mode)
 {
+#ifndef EDITOR
 	SDL_Surface* sprite = nullptr;
 	real_t s = 1;
 
@@ -1525,6 +1526,7 @@ void glDrawWorldUISprite(view_t* camera, Entity* entity, int mode)
 			sprite = nullptr;
 		}
 	}
+#endif
 }
 
 void glDrawSprite(view_t* camera, Entity* entity, int mode)
