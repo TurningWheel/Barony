113358b4c73040033d5d9581ea2dbd4950947854
SheridanR <sheridan.rathbun@gmail.com>
2022-10-31 22:47:28 -0700

    fix some more bugaroos

--- src/ui/MainMenu.hpp
+++ src/ui/MainMenu.hpp
@@ -75,6 +75,7 @@ namespace MainMenu {
 
 	int getMenuOwner();					// get current pause menu owner
     bool isPlayerSignedIn(int index);   // checks whether a player is signed into a given slot
+	bool isPlayerSlotLocked(int index);	// checks whether a player slot has been locked out for joining
     bool isCutsceneActive();            // checks whether we are playing a cutscene
 	void beginFade(FadeDestination);    // begins a fade transition to a specific destination
 
