0cad49f900bd65464fb429856fd7d2bcab324ea5
WALL OF JUSTICE <->
2022-11-06 02:13:18 +1100

    * reduce opengl req to 4.0, gamma seems fine and shaders compile successfully

--- src/init.cpp
+++ src/init.cpp
@@ -2570,7 +2570,7 @@ bool initVideo()
 {
     if (!renderer) {
 	    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-	    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+	    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 	    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
 	    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
 	    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
