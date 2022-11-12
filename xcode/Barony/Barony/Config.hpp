e52e7e8e036e8db34f4a5f85c85ccc74726d1c13
SheridanR <sheridan.rathbun@gmail.com>
2022-11-12 14:32:49 -0800

    xcode project
    Signed-off-by: SheridanR <sheridan.rathbun@gmail.com>

---
+++ xcode/Barony/Barony/Config.hpp
@@ -0,0 +1,10 @@
+#define APPLE
+
+#define USE_FMOD
+//#define STEAMWORKS
+//#define USE_EOS
+//#define USE_THEORA_PLAYER
+//#define USE_IMGUI
+
+#define EDITOR_EXE_NAME "editor"
+#define BASE_DATA_DIR "./"
