769beec0e0fe834886009b0c779185ad0b32cb9c
WALL OF JUSTICE <->
2022-11-14 13:36:05 +1100

    * remove old gui checks for click detection, now just use frameseatmouse * right click with no tooltips in view don't try find an active tooltip (perf optimization)

--- src/interface/clickdescription.cpp
+++ src/interface/clickdescription.cpp
@@ -18,6 +18,7 @@
 #include "../net.hpp"
 #include "../player.hpp"
 #include "interface.hpp"
+#include "../ui/GameUI.hpp"
 
 /*-------------------------------------------------------------------------------
 
@@ -37,6 +38,10 @@ void clickDescription(int player, Entity* entity)
 
 	if ( entity == NULL )
 	{
+		if ( !framesProcResult.usable && *framesEatMouse )
+		{
+			return;
+		}
 		if ( !input.binaryToggle("InspectWithMouse") || players[player]->shootmode )
 		{
 			return;
@@ -48,130 +53,8 @@ void clickDescription(int player, Entity* entity)
 
 		auto& camera = cameras[player];
 
-		//One of either IN_ATTACK or INJOY_MENU_LEFT_CLICK is true, && shootmode == false;
 		if ( mx < camera.winx || mx >= camera.winx + camera.winw || my < camera.winy || my >= camera.winy + camera.winh )
 		{
-			return;
-		}
-		if ( FollowerMenu[player].followerMenuIsOpen() )
-		{
-			return;
-		}
-		if ( openedChest[player] )
-		{
-			// TODO UI: REPLACE
-			//if ( mx > getChestGUIStartX(player)
-			//	&& mx < getChestGUIStartX(player) + inventoryChest_bmp->w
-			//	&& my > getChestGUIStartY(player)
-			//	&& my < getChestGUIStartY(player) + inventoryChest_bmp->h )
-			//{
-			//	return;    //Click falls inside the chest inventory GUI.
-			//}
-		}
-		SDL_Rect guiBox;
-		GenericGUI[player].getDimensions(guiBox);
-		if ( GenericGUI[player].isGUIOpen() )
-		{
-			if ( mx > guiBox.x
-				&& mx < guiBox.x + guiBox.w
-				&& my > guiBox.y
-				&& my < guiBox.y + guiBox.h )
-			{
-				return;    //Click falls inside the generic gui.
-			}
-		}
-		if ( players[player]->bookGUI.bBookOpen )
-		{
-			if ( mouseInBounds(player,
-				players[player]->bookGUI.getStartX(), 
-				players[player]->bookGUI.getStartX() + players[player]->bookGUI.getBookWidth(),
-				players[player]->bookGUI.getStartY(), players[player]->bookGUI.getStartY() + players[player]->bookGUI.getBookHeight()) )
-			{
-				return;    //Click falls inside the book GUI.
-			}
-		}
-		if ( players[player]->gui_mode == GUI_MODE_INVENTORY || players[player]->gui_mode == GUI_MODE_SHOP)
-		{
-			if ( players[player]->gui_mode == GUI_MODE_INVENTORY )
-				if (mouseInBounds(player, RIGHTSIDEBAR_X, RIGHTSIDEBAR_X + rightsidebar_titlebar_img->w, RIGHTSIDEBAR_Y, RIGHTSIDEBAR_Y + rightsidebar_height))
-				{
-					return;    //Click falls inside the right sidebar.
-				}
-
-			if ( players[player]->skillSheet.bSkillSheetOpen )
-			{
-				return;
-			}
-			//int x = std::max(character_bmp->w, xres/2-inventory_bmp->w/2);
-			//if (mouseInBounds(x,x+inventory_bmp->w,0,inventory_bmp->h))
-			//return NULL;
-
-			// TODO UI: REPLACE
-			//if ( mouseInBounds(player, 
-			//	inventoryUI.getStartX(),
-			//	inventoryUI.getStartX() + inventoryUI.getSizeX() * inventoryUI.getSlotSize(),
-			//	inventoryUI.getStartY(),
-			//	inventoryUI.getStartY() + inventoryUI.getSizeY() * inventoryUI.getSlotSize()) )
-			//{
-			//	// clicked in inventory
-			//	return;
-			//}
-			if ( players[player]->gui_mode == GUI_MODE_SHOP )
-			{
-				int x1 = xres / 2 - SHOPWINDOW_SIZEX / 2, x2 = xres / 2 + SHOPWINDOW_SIZEX / 2;
-				int y1 = yres / 2 - SHOPWINDOW_SIZEY / 2, y2 = yres / 2 + SHOPWINDOW_SIZEY / 2;
-				if (mouseInBounds(player, x1, x2, y1, y2))
-				{
-					return;
-				}
-			}
-		}
-		//else if ( players[player]->gui_mode == GUI_MODE_MAGIC)
-		//{
-		//	if (magic_GUI_state == 0)
-		//	{
-		//		//Right, now calculate the spell list's height (the same way it calculates it for itself).
-		//		int height = spell_list_titlebar_bmp->h;
-		//		int numspells = 0;
-		//		node_t* node;
-		//		for (node = players[player]->magic.spellList.first; node != NULL; node = node->next)
-		//		{
-		//			numspells++;
-		//		}
-		//		int maxSpellsOnscreen = yres / spell_list_gui_slot_bmp->h;
-		//		numspells = std::min(numspells, maxSpellsOnscreen);
-		//		height += numspells * spell_list_gui_slot_bmp->h;
-		//		int spelllist_y = 0 + ((yres / 2) - (height / 2)) + magicspell_list_offset_x;
-
-		//		if (mouseInBounds(player, MAGICSPELL_LIST_X, MAGICSPELL_LIST_X + spell_list_titlebar_bmp->w, spelllist_y, spelllist_y + height))
-		//		{
-		//			return;
-		//		}
-		//	}
-		//}
-
-		SDL_Rect& interfaceCharacterSheet = players[player]->characterSheet.characterSheetBox;
-		SDL_Rect& interfaceMessageStatusBar = players[player]->statusBarUI.messageStatusBarBox;
-		SDL_Rect& interfaceSkillsSheet = players[player]->characterSheet.skillsSheetBox;
-		SDL_Rect& interfacePartySheet = players[player]->characterSheet.partySheetBox;
-		SDL_Rect& interfaceStatsSheet = players[player]->characterSheet.statsSheetBox;
-		if ( mouseInBounds(player, interfaceCharacterSheet.x, interfaceCharacterSheet.x + interfaceCharacterSheet.w,
-			interfaceCharacterSheet.y, interfaceCharacterSheet.y + interfaceCharacterSheet.h) )
-		{
-			// character sheet
-			return;
-		}
-		if ( mouseInBounds(player, interfaceStatsSheet.x, interfaceStatsSheet.x + interfaceStatsSheet.w,
-			interfaceStatsSheet.y, interfaceStatsSheet.y + interfaceStatsSheet.h) )
-		{
-			// stats sheet
-			return;
-		}
-		if (!hide_statusbar && 
-			mouseInBounds(player, interfaceMessageStatusBar.x, interfaceMessageStatusBar.x + interfaceMessageStatusBar.w,
-				interfaceMessageStatusBar.y, interfaceMessageStatusBar.y + interfaceMessageStatusBar.h) )
-		{
-			// bottom message log
 			return;
 		}
 
@@ -181,28 +64,10 @@ void clickDescription(int player, Entity* entity)
 			return;
 		}
 
-		// ui code taken from drawSkillsSheet() and drawPartySheet().
-		if ( players[player]->characterSheet.proficienciesPage == 0 )
-		{
-			if ( mouseInBounds(player, interfaceSkillsSheet.x, interfaceSkillsSheet.x + interfaceSkillsSheet.w,
-				interfaceSkillsSheet.y, interfaceSkillsSheet.y + interfaceSkillsSheet.h) )
-			{
-				return;
-			}
-		}
-		else
-		{
-			if ( mouseInBounds(player, interfacePartySheet.x, interfacePartySheet.x + interfacePartySheet.w,
-				interfacePartySheet.y, interfacePartySheet.y + interfacePartySheet.h) )
-			{
-				return;
-			}
-		}
-
-		if ( mouseInsidePlayerInventory(player) || mouseInsidePlayerHotbar(player) )
+		/*if ( mouseInsidePlayerInventory(player) || mouseInsidePlayerHotbar(player) )
 		{
 			return;
-		}
+		}*/
 
 		input.consumeBinaryToggle("InspectWithMouse");
 
