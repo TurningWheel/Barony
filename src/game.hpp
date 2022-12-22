03407fdd898fc0c7570880328914d6ec097ed4a2
WALL OF JUSTICE <->
2022-12-23 01:10:59 +1100

    * game version bump new models for hell

--- src/game.hpp
+++ src/game.hpp
@@ -22,7 +22,7 @@
 #include "interface/consolecommand.hpp"
 
 // REMEMBER TO CHANGE THIS WITH EVERY NEW OFFICIAL VERSION!!!
-static const char VERSION[] = "v3.8.5";
+static const char VERSION[] = "v3.8.6";
 #define GAME_CODE
 
 class Entity;
