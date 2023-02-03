a08a227ec1d0c203c6c6d046c649341a0d90033b
WALL OF JUSTICE <->
2023-02-04 05:13:26 +1100

    * version increase

--- src/game.hpp
+++ src/game.hpp
@@ -22,7 +22,7 @@
 #include "interface/consolecommand.hpp"
 
 // REMEMBER TO CHANGE THIS WITH EVERY NEW OFFICIAL VERSION!!!
-static const char VERSION[] = "v3.8.6";
+static const char VERSION[] = "v3.8.7";
 #define GAME_CODE
 
 class Entity;
