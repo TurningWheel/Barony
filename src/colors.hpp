ba4e168b03479e38b65978ae95170b891618845e
SheridanR <sheridan.rathbun@gmail.com>
2023-02-20 19:02:48 -0800

    Fix ally colors on minimap
    Signed-off-by: SheridanR <sheridan.rathbun@gmail.com>

--- src/colors.hpp
+++ src/colors.hpp
@@ -69,11 +69,11 @@ constexpr Uint32 playerColor(int index, bool colorblind, bool ally) {
 		}
 		else {
 			switch (index) {
-			default: return uint32ColorPlayerX;
-			case 0: return uint32ColorPlayer1;
-			case 1: return uint32ColorPlayer2;
-			case 2: return uint32ColorPlayer3;
-			case 3: return uint32ColorPlayer4;
+			default: return uint32ColorPlayerX_Ally;
+			case 0: return uint32ColorPlayer1_Ally;
+			case 1: return uint32ColorPlayer2_Ally;
+			case 2: return uint32ColorPlayer3_Ally;
+			case 3: return uint32ColorPlayer4_Ally;
 			}
 		}
 	} else {
