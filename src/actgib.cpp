8b871635afe870a4e083fdadd275a0b03458e70a
SheridanR <sheridan.rathbun@gmail.com>
2023-04-30 22:40:52 -0700

    gibs have dithering disabled
    Signed-off-by: SheridanR <sheridan.rathbun@gmail.com>

--- src/actgib.cpp
+++ src/actgib.cpp
@@ -284,6 +284,7 @@ Entity* spawnGib(Entity* parentent, int customGibSprite)
 	entity->vel_z = -.5;
 	entity->fskill[3] = 0.04;
 	entity->behavior = &actGib;
+    entity->ditheringDisabled = true;
 	entity->flags[PASSABLE] = true;
 	entity->flags[NOUPDATE] = true;
 	entity->flags[UNCLICKABLE] = true;
