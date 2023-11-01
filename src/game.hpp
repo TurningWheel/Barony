81632d7d13f3372d045ae669643b976183c29933
SheridanR <sheridan.rathbun@gmail.com>
2023-11-01 14:43:12 -0700

    version bump
    Signed-off-by: SheridanR <sheridan.rathbun@gmail.com>

--- src/game.hpp
+++ src/game.hpp
@@ -25,9 +25,9 @@
 
 // REMEMBER TO CHANGE THIS WITH EVERY NEW OFFICIAL VERSION!!!
 #ifdef NINTENDO
-static const char VERSION[] = "v4.1.0";
+static const char VERSION[] = "v4.1.1";
 #else
-static const char VERSION[] = "v4.1.0";
+static const char VERSION[] = "v4.1.1";
 #endif
 #define GAME_CODE
 
