6ba87fee6aab8d4a773d1ec6174896588b54fea4
SheridanR <sheridan.rathbun@gmail.com>
2024-02-29 13:16:03 -0800

    fix linux compilation

--- cmake/Modules/FindPLAYFAB.cmake
+++ cmake/Modules/FindPLAYFAB.cmake
@@ -1,56 +1,30 @@
-# - Try to find the PlayFab C++ SDK
-#
-# Once done, this will define:
-#
-#  PLAYFAB_INCLUDE_DIR - the PlayFab include directory
-#  PLAYFAB_LIBRARIES - The libraries needed to use PlayFab
 
-if (NOT PLAYFAB_INCLUDE_DIR OR NOT PLAYFAB_LIBRARIES)
-	set(LIB_SEARCH_PATHS
-		~/Library/Frameworks
-		/Library/Frameworks
-		/usr/lib
-		/usr/lib64
-		/usr/local/lib
-		/usr/local/lib64
-		$ENV{PLAYFAB_ROOT}/SDK
-		$ENV{PLAYFAB_DIR}/SDK
-	)
-	FIND_PATH(PLAYFAB_INCLUDE_DIR PlayFabAdminApi.h
-		/usr/include
-		/usr/local/include
-		$ENV{PLAYFAB_ROOT}/include/playfab
-		$ENV{PLAYFAB_DIR}/include/playfab
-		DOC "Include path for PlayFab"
-	)
+find_path(PLAYFAB_INCLUDE_DIR playfab/PlayFabAdminApi.h
+  HINTS
+    ENV PLAYFAB_DIR
+  PATH_SUFFIXES include/playfab include
+  PATHS
+  ~/Library/Frameworks
+  /Library/Frameworks
+  /sw # Fink
+  /opt/local # DarwinPorts
+  /opt/csw # Blastwave
+  /opt
+)
 
-	if (Windows)
-		FIND_LIBRARY(PLAYFAB_LIBRARY NAMES playfab.lib
-			PATHS
-			$ENV{PLAYFAB_ROOT}/lib
-			$ENV{PLAYFAB_DIR}/lib
-			DOC "PlayFab library name"
-		)
-	elseif (APPLE)
-		FIND_LIBRARY(PLAYFAB_LIBRARY NAMES playfab.a
-			PATHS
-			$ENV{PLAYFAB_ROOT}/lib
-			$ENV{PLAYFAB_DIR}/lib
-			DOC "PlayFab library name"
-		)
-	else ()
-		FIND_LIBRARY(PLAYFAB_LIBRARY NAMES playfab.a
-			PATHS
-			$ENV{PLAYFAB_ROOT}/lib
-			$ENV{PLAYFAB_DIR}/lib
-			DOC "PlayFab library name"
-		)
-	endif ()
-	if (PLAYFAB_LIBRARY)
-		set(PLAYFAB_LIBRARIES ${PLAYFAB_LIBRARY})
-	endif ()
-	MARK_AS_ADVANCED(PLAYFAB_INCLUDE_DIR PLAYFAB_LIBRARIES)
-endif ()
+find_library(PLAYFAB_LIBRARY
+  NAMES playfab
+  HINTS
+    ENV PLAYFAB_DIR
+  PATH_SUFFIXES lib lib64
+  PATHS
+  ~/Library/Frameworks
+  /Library/Frameworks
+  /sw
+  /opt/local
+  /opt/csw
+  /opt
+)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(PLAYFAB DEFAULT_MSG PLAYFAB_INCLUDE_DIR PLAYFAB_LIBRARIES)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(PLAYFAB DEFAULT_MSG PLAYFAB_LIBRARY PLAYFAB_INCLUDE_DIR)
