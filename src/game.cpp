7360fb4151bd6c28f202d620a30a462199033efe
Damglador <vse.stopchanskyi@gmail.com>
2025-05-30 17:42:08 +0200

    Move data directory to .local/share/barony
    Technically it is not how XDG dictates it should be, but for most users that will work just fine

--- src/game.cpp
+++ src/game.cpp
@@ -6759,11 +6759,11 @@ int main(int argc, char** argv)
   #ifdef USE_EOS
    #ifdef STEAMWORKS
 		//Steam + EOS
-		snprintf(outputdir, sizeof(outputdir), "%s/.barony", basepath);
+		snprintf(outputdir, sizeof(outputdir), "%s/.local/share/barony", basepath);
    #else
 		//Just EOS.
 		std::string firstDotdir(basepath);
-		firstDotdir += "/.barony/";
+		firstDotdir += "/.local/share/barony/";
 		if (access(firstDotdir.c_str(), F_OK) == -1)
 		{
 			mkdir(firstDotdir.c_str(), 0777); //Since this mkdir is not equivalent to mkdir -p, have to create each part of the path manually.
@@ -6772,7 +6772,7 @@ int main(int argc, char** argv)
    #endif
   #else //USE_EOS
 		//No EOS. Could be Steam though. Or could also not.
-		snprintf(outputdir, sizeof(outputdir), "%s/.barony", basepath);
+		snprintf(outputdir, sizeof(outputdir), "%s/.local/share/barony", basepath);
   #endif
 		if (access(outputdir, F_OK) == -1)
 		{
